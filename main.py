44bd6fefec4f2760d80d7051537eb00504142d17
chemi5t <umar.chemi5t@gmail.com>
2024-07-27 00:06:02 +0100

    Test main.py and pip install anything required.

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import os
-from decouple import config # Calling sensitive information
+import pandas as pd
 
-
+from decouple import config  # For managing sensitive information
 from _01_emis_data_analysis_project_files.data_extraction import DataExtractor as de
 from _01_emis_data_analysis_project_files.database_utils import DatabaseConnector as dco
 
-
-
-def print_dataframe_info(df):
+def print_dataframe_info(df: pd.DataFrame):
     """
     Prints the DataFrame table, its info, and description.
 
     Parameters:
-    df (pandas.DataFrame): The DataFrame to be printed and analysed.
+    df (pandas.DataFrame): The DataFrame to be printed and analyzed.
 
     Returns:
     None
@@ -32,15 +30,16 @@ if __name__ == "__main__":
     data_extractor = de()
     data_connector = dco()
 
-    cred_config_access = config('credentials_env') # refers to .yaml file via decouple import config; to gain access to private credentials for API
-    cred_config_api = data_connector.read_db_creds(file_path = cred_config_access) # extracts the credentials from .yaml file
+    # Read credentials from environment file
+    cred_config_access = config('credentials_env')  # Refers to .yaml file via decouple
+    cred_config_api = data_connector.read_db_creds(file_path=cred_config_access)  # Extract credentials from .yaml file
 
-    postgres_engine = data_connector.init_db_engine(credentials = cred_config_api)      # Initialise database engine
+    # Initialise database engine
+    postgres_engine = data_connector.init_db_engine(credentials=cred_config_api)
 
+    print("################################################## Medication Extraction ##################################################")
 
-    print("################################################## medication extraction ##################################################")
-
-    # Define 1. the folder path containing the CSV files and 2. the header file name
+    # Define folder path and header file for medication data
     folder_path = os.path.join('data', 'medication')
     header_file = 'medication.csv'
 
@@ -49,17 +48,13 @@ if __name__ == "__main__":
 
     # Save the combined DataFrame to a new CSV file
     medication_df.to_csv(os.path.join('data', 'combined_medication.csv'), index=False, encoding='utf-8')
-
     print("\nNote: Created 'combined_medication.csv' and saved to 'data' folder.\n")
 
     print_dataframe_info(medication_df)
 
-    print("################################################## observation extraction ##################################################")
+    print("################################################## Observation Extraction ##################################################")
 
-    # NOTE: following error was noted [C:\Users\chemi\AiCore_Projects\exa-data-analyst-assessment\_01_emis_data_analysis_project_files\data_extraction.py:45: DtypeWarning: Columns (2) have mixed types. Specify dtype option on import or set low_memory=False.]
-    #       Due to this, dtype was inferred during the extracting of all the .csv files. Note the issue was within the observation records column 2. I would think it needs to be an integer.
-
-    # Define 1. the folder containing the CSV files and 2. the header file name
+    # Define folder path and header file for observation data
     folder_path = os.path.join('data', 'observation')
     header_file = 'observation.csv'
 
@@ -68,28 +63,26 @@ if __name__ == "__main__":
 
     # Save the combined DataFrame to a new CSV file
     observation_df.to_csv(os.path.join('data', 'combined_observation.csv'), index=False, encoding='utf-8')
-
     print("\nNote: Created 'combined_observation.csv' and saved to 'data' folder.\n")
 
     print_dataframe_info(observation_df)
 
-    print("################################################## clinical extraction ##################################################")
+    print("################################################## Clinical Extraction ##################################################")
 
-    # Define the path to the clinical codes CSV file
+    # Define path to clinical codes CSV file
     clinical_codes_file_path = os.path.join('data', 'clinical_codes.csv')
 
-    # Extract clinical_codes from the CSV file into a pandas DataFrame
+    # Extract clinical codes from CSV file
     clinical_codes_df = data_extractor.extract_from_csv(clinical_codes_file_path)
 
     print_dataframe_info(clinical_codes_df)
 
-    print("################################################## patient extraction ##################################################")
+    print("################################################## Patient Extraction ##################################################")
 
-    # Define the path to the CSV file
+    # Define path to patient CSV file
     patient_file_path = os.path.join('data', 'patient.csv')
 
-    # Extract data from the CSV file into a pandas DataFrame
+    # Extract patient data from CSV file
     patient_df = data_extractor.extract_from_csv(patient_file_path)
 
     print_dataframe_info(patient_df)
- 
