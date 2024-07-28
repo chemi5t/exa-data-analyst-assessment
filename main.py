2eaf8230105f6323d43f526b08e187d67f2a8cff
chemi5t <umar.chemi5t@gmail.com>
2024-07-28 12:29:59 +0100

    Create functions for code run in main.py under if __name__ == __main__ row.

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ from _01_emis_data_analysis_project_files.database_utils import DatabaseConnecto
 from _01_emis_data_analysis_project_files.data_cleaning import DataCleaning as dcl
 from _01_emis_data_analysis_project_files.data_extraction import DataExtractor as de
 
+print("################################################## Initialise instances ##################################################")
+
+# Initialise instances
+data_extractor = de()
+data_connector = dco()
+data_cleaning = dcl()  
+
+# Read credentials from environment file
+cred_config_access = config('credentials_env')  # Refers to .yaml file via decouple
+cred_config_api = data_connector.read_db_creds(file_path=cred_config_access)  # Extract credentials from .yaml file
+
+# Initialise database engine
+postgres_engine = data_connector.init_db_engine(credentials=cred_config_api)
 
 def print_dataframe_info(df: pd.DataFrame):
     """
@@ -26,77 +39,85 @@ def print_dataframe_info(df: pd.DataFrame):
     print("\nDataFrame.describe(): \n")
     print(df.describe())
 
+def medication_extraction():
+    # Define folder path and header file for medication data
+    folder_path = os.path.join('data', 'medication')
+    header_file = 'medication.csv'
+
+    # Read and combine CSV files
+    medication_df = data_extractor.read_and_combine_csv(folder_path, header_file)
+
+    # Save the combined DataFrame to a new CSV file
+    medication_df.to_csv(os.path.join('data', 'combined_medication.csv'), index=False, encoding='utf-8')
+    print("\nNote: Created 'combined_medication.csv' and saved to 'data' folder.\n")
+
+    print_dataframe_info(medication_df)
+
+def observation_extraction():
+
+    # Define folder path and header file for observation data
+    folder_path = os.path.join('data', 'observation')
+    header_file = 'observation.csv'
+
+    # Read and combine CSV files
+    observation_df = data_extractor.read_and_combine_csv(folder_path, header_file)
+
+    # Save the combined DataFrame to a new CSV file
+    observation_df.to_csv(os.path.join('data', 'combined_observation.csv'), index=False, encoding='utf-8')
+    print("\nNote: Created 'combined_observation.csv' and saved to 'data' folder.\n")
+
+    # Clean the 'comparator' column in the 'observation' DataFrame using the DataCleaning class.
+    # The method ensures that the column is treated as a string, replaces 'nan' strings with actual NaN values,
+    # and converts the column dtype to 'category' from 'object' to handle mixed dtype issues.
+    observation_df = data_cleaning.clean_column_data(observation_df, 'comparator')
+
+    print("The 'comparator' column has been cleaned using the 'data_cleaning' method. The dtype has been cast to 'category' from 'object' to resolve any mixed dtype issues.\n")
+
+    print_dataframe_info(observation_df)
+
+
+def clinical_extraction():
+    # Define path to clinical codes CSV file
+    clinical_codes_file_path = os.path.join('data', 'clinical_codes.csv')
+
+    # Extract clinical codes from CSV file
+    clinical_codes_df = data_extractor.extract_from_csv(clinical_codes_file_path)
+
+    print_dataframe_info(clinical_codes_df)
+
+def patient_extraction():
+    # Define path to patient CSV file
+    patient_file_path = os.path.join('data', 'patient.csv')
+
+    # Extract patient data from CSV file
+    patient_df = data_extractor.extract_from_csv(patient_file_path)
+
+    print_dataframe_info(patient_df)
+
 if __name__ == "__main__":
 
     try:
-        # Initialise instances
-        data_extractor = de()
-        data_connector = dco()
-        data_cleaning = dcl()  
-
-        # Read credentials from environment file
-        cred_config_access = config('credentials_env')  # Refers to .yaml file via decouple
-        cred_config_api = data_connector.read_db_creds(file_path=cred_config_access)  # Extract credentials from .yaml file
-
-        # Initialise database engine
-        postgres_engine = data_connector.init_db_engine(credentials=cred_config_api)
-
         print("################################################## Medication Extraction ##################################################")
 
-        # Define folder path and header file for medication data
-        folder_path = os.path.join('data', 'medication')
-        header_file = 'medication.csv'
-
-        # Read and combine CSV files
-        medication_df = data_extractor.read_and_combine_csv(folder_path, header_file)
-
-        # Save the combined DataFrame to a new CSV file
-        medication_df.to_csv(os.path.join('data', 'combined_medication.csv'), index=False, encoding='utf-8')
-        print("\nNote: Created 'combined_medication.csv' and saved to 'data' folder.\n")
-
-        print_dataframe_info(medication_df)
+        medication_extraction()
 
         print("################################################## Observation Extraction ##################################################")
 
-        # Define folder path and header file for observation data
-        folder_path = os.path.join('data', 'observation')
-        header_file = 'observation.csv'
-
-        # Read and combine CSV files
-        observation_df = data_extractor.read_and_combine_csv(folder_path, header_file)
-
-        # Save the combined DataFrame to a new CSV file
-        observation_df.to_csv(os.path.join('data', 'combined_observation.csv'), index=False, encoding='utf-8')
-        print("\nNote: Created 'combined_observation.csv' and saved to 'data' folder.\n")
-        
-        # Clean the 'comparator' column in the 'observation' DataFrame using the DataCleaning class.
-        # The method ensures that the column is treated as a string, replaces 'nan' strings with actual NaN values,
-        # and converts the column dtype to 'category' from 'object' to handle mixed dtype issues.
-        observation_df = data_cleaning.clean_column_data(observation_df, 'comparator')
-
-        print("The 'comparator' column has been cleaned using the 'data_cleaning' method. The dtype has been cast to 'category' from 'object' to resolve any mixed dtype issues.\n")
-
-        print_dataframe_info(observation_df)
+        observation_extraction()
 
         print("################################################## Clinical Extraction ##################################################")
 
-        # Define path to clinical codes CSV file
-        clinical_codes_file_path = os.path.join('data', 'clinical_codes.csv')
-
-        # Extract clinical codes from CSV file
-        clinical_codes_df = data_extractor.extract_from_csv(clinical_codes_file_path)
-
-        print_dataframe_info(clinical_codes_df)
+        clinical_extraction()
 
         print("################################################## Patient Extraction ##################################################")
 
-        # Define path to patient CSV file
-        patient_file_path = os.path.join('data', 'patient.csv')
+        patient_extraction()
 
-        # Extract patient data from CSV file
-        patient_df = data_extractor.extract_from_csv(patient_file_path)
+        print("################################################## Upload to postgresSQL (pgAdmin4) ##################################################")
 
-        print_dataframe_info(patient_df)
+
+
+
 
     except FileNotFoundError as e:
         print(f"Error: {e}")
